5b9642742e83b4737ed4f61bbde56891ee3e73b8
Rob Phoenix <rob@robphoenix.com>
2019-05-06 18:23:39 +0100

    Clean up design tokens

--- src/utils/tokens.js
+++ src/utils/tokens.js
@@ -1,45 +1,39 @@
-const tokens = {
-  breakpoints: [],
-  mediaQueries: [],
-  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
-  fontSizes: [0, 12, 14, 16, 18, 24, 32, 48, 64, 72],
-  fontWeights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-  lineHeights: {
-    solid: 1,
-    title: 1.25,
-    copy: 1.5,
-  },
-  letterSpacings: {
-    normal: "normal",
-    tracked: "0.1em",
-    tight: "-0.05em",
-    mega: "0.25em",
-  },
-  fonts: {
-    body: "Overpass, sans-serif",
-  },
-  heights: [0, 16, 24, 32, 64, 128, 256],
-  maxWidths: [16, 32, 64, 128, 256, 512, 768, 1024, 1536],
-  colors: {
-    primary: "#ffd43b",
-    text: `#551400`,
-  },
+const space = {
+  xs: `4px`,
+  sm: `8px`,
+  md: `16px`,
+  lg: `32px`,
+  xl: `64px`,
+  xxl: `128px`,
+  xxxl: `256px`,
+}
+
+const colours = {
+  background: "#ffd43b",
+  text: `#303030`,
+}
+
+const fonts = {
+  body: "Overpass, sans-serif",
+}
+
+const fontSizes = {
+  sm: `1rem`,
+  md: `1.25rem`,
+  lg: `1.5rem`,
+  xl: `2rem`,
+  xxl: `2.5rem`,
 }
 
 const breakpoints = {
-  xs: `400px`,
-  sm: `550px`,
-  md: `750px`,
-  lg: `1000px`,
-  xl: `1200px`,
-  xxl: `1600px`,
+  sm: `600px`,
+  md: `900px`,
+  lg: `1200px`,
 }
 
-for (let breakpoint in breakpoints) {
-  tokens.mediaQueries[breakpoint] = `@media (min-width: ${
-    breakpoints[breakpoint]
-  })`
-  tokens.breakpoints.push(breakpoints[breakpoint])
-}
+const mediaQueries = Object.keys(breakpoints).reduce((prev, breakpoint) => {
+  prev[breakpoint] = `@media (min-width: ${breakpoints[breakpoint]})`
+  return prev
+}, {})
 
-export default tokens
+export { breakpoints, mediaQueries, colours, fonts, space, fontSizes }
